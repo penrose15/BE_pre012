8c43338dab59b006b45c86dfae4fd2ec67f13324
penrose15 <seongju99@naver.com>
2022-08-24 14:51:53 +0900

    member join

--- src/main/java/com/codestates/pre012/member/mapper/MemberMapper.java
+++ src/main/java/com/codestates/pre012/member/mapper/MemberMapper.java
@@ -1,7 +1,16 @@
 package com.codestates.pre012.member.mapper;
 
+import com.codestates.pre012.member.dto.PostMemberDto;
+import com.codestates.pre012.member.dto.ResponseMemberDto;
+import com.codestates.pre012.member.entity.Member;
 import org.mapstruct.Mapper;
+import org.springframework.http.ResponseEntity;
+
 //mapper어노테이션 추가
 @Mapper(componentModel = "spring")
 public interface MemberMapper {
+
+    Member PostMemberDtoToMember(PostMemberDto postMemberDto);
+
+    ResponseMemberDto MemberToResponseMemberDto(Member member);
 }
--- src/main/java/com/codestates/pre012/member/service/MemberService.java
+++ src/main/java/com/codestates/pre012/member/service/MemberService.java
@@ -1,8 +1,12 @@
 package com.codestates.pre012.member.service;
 
 
+import com.codestates.pre012.member.dto.LoginMemberDto;
+import com.codestates.pre012.member.entity.Member;
 import com.codestates.pre012.member.repository.MemberRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 //memberservice 추가, repository 의존성 주입
 @Service
@@ -11,5 +15,35 @@ public class MemberService {
 
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
+    }
+    //member 회원가입
+    public Member savedMember(Member member) {
+        System.out.println(member.getEmail() +"/"+ member.getPassword());
+        verifiedMemberEmail(member.getEmail());
+
+        return memberRepository.save(member);
+    }
+    public Member login(LoginMemberDto loginMemberDto) {
+        Member member = loginVerifiedEmail(loginMemberDto.getEmail());
+        if(member.getPassword().equals(loginMemberDto.getPassword())) {
+            return member;
+        }
+        else {
+            throw new RuntimeException("wrong password!!");
+        }
+    }
+
+
+    //이메일 존재시 예외처리
+    public void verifiedMemberEmail(String email) {
+        Optional<Member> verifyMember = memberRepository.findByEmail(email);
+        if(verifyMember.isPresent()) throw new RuntimeException("member Already Exist");
+    }
+
+    public Member loginVerifiedEmail(String email) {
+        Optional<Member> loginMember = memberRepository.findByEmail(email);
+        Member member = loginMember.orElseThrow(() -> new RuntimeException("email not exist"));
+
+        return member;
     }
 }
