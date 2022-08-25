278943d563dd248f30d8331b0f93e15e32175f5e
Choonsik <moodeary@gmail.com>
2022-08-25 12:19:38 +0900

    Controller updated

--- src/main/java/com/codestates/pre012/posts/entity/Posts.java
+++ src/main/java/com/codestates/pre012/posts/entity/Posts.java
@@ -1,22 +1,36 @@
 package com.codestates.pre012.posts.entity;
 
+import com.codestates.pre012.baseEntity.BaseEntity;
 import com.codestates.pre012.member.entity.Member;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
 
 @Entity
-public class Posts {
+@Getter
+@Setter
+@NoArgsConstructor
+public class Posts extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long postId;
+    private long postsId;
 
     @Column(name = "title", nullable = false)
     private String title;
 
-    private String test;
-
     @Column(name = "content", nullable = false)
     private String content;
-    
+
+    @JoinColumn(name = "memberId")
+    @ManyToOne
+    private Member member;
+
+    public Posts(long postsId, String title, String content) {
+        this.postsId = postsId;
+        this.title = title;
+        this.content = content;
+    }
 }
--- src/main/java/com/codestates/pre012/posts/repository/PostsRepository.java
+++ src/main/java/com/codestates/pre012/posts/repository/PostsRepository.java
@@ -4,4 +4,6 @@ import com.codestates.pre012.posts.entity.Posts;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostsRepository extends JpaRepository<Posts, Long> {
+
+
 }
