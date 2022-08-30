88b34099868c0f2443c3d41f3231de74f43e5204
Choonsik <moodeary@gmail.com>
2022-08-30 13:02:20 +0900

    - jwt,oauth2 구현, test 임시 제거 - MemberDto Validation 활성화 - GlobalException 활성화

--- src/main/java/com/codestates/pre012/advice/GlobalExceptionAdvice.java
+++ src/main/java/com/codestates/pre012/advice/GlobalExceptionAdvice.java
@@ -3,7 +3,10 @@ package com.codestates.pre012.advice;
 import com.codestates.pre012.exception.BusinessLogicException;
 import com.codestates.pre012.response.ErrorResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -52,7 +55,15 @@ public class GlobalExceptionAdvice {
         return new ResponseEntity<>(valueOf(e.getExceptionCode().getStatus()));
     }
 
-    
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.METHOD_NOT_ALLOWED)
+    public ErrorResponse handleHttpRequestMethodNotSupportedException(
+            HttpRequestMethodNotSupportedException e) {
+
+        final ErrorResponse response = ErrorResponse.of(HttpStatus.METHOD_NOT_ALLOWED);
+
+        return response;
+    }
 
 }
 
