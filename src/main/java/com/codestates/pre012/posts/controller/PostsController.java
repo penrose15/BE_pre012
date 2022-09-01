b9bf86b1794d0999c0d9fc2b86f638313098c9e4
penrose15 <seongju99@naver.com>
2022-09-01 15:54:30 +0900

    PostController conflict 해결

--- src/main/java/com/codestates/pre012/posts/controller/PostsController.java
+++ src/main/java/com/codestates/pre012/posts/controller/PostsController.java
@@ -8,11 +8,6 @@ import com.codestates.pre012.posts.dto.PostsDto;
 import com.codestates.pre012.posts.entity.Posts;
 import com.codestates.pre012.posts.mapper.PostsMapper;
 import com.codestates.pre012.posts.service.PostsService;
-import com.codestates.pre012.reply.ReplyMapper;
-import com.codestates.pre012.reply.ReplyService;
-import com.codestates.pre012.tag.TagMapper;
-import com.codestates.pre012.tag.dto.TagDto;
-import com.codestates.pre012.tag.entity.Tag;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -48,26 +43,24 @@ public class PostsController {
         return new ResponseEntity<>(new SingleResponseDto<>(mapper.postsToPostsDtoResponse(response)), HttpStatus.CREATED);
     }
 
-
     @PatchMapping("/patch")
-    public ResponseEntity patchPosts(@Valid @RequestBody PostsDto.Patch posts, @AuthenticationPrincipal PrincipalDetails principal) {
+    public ResponseEntity patchPosts(@Valid @RequestBody PostsDto.Patch posts
+            ,@AuthenticationPrincipal PrincipalDetails principal) {
 
 
         posts.setPostsId(posts.getPostsId());
-        Posts response = postsService.updatePosts(mapper.postsPatchDtoToPosts(posts), principal.getMember());
+        Posts response = postsService.updatePosts(
+                mapper.postsPatchDtoToPosts(posts)
+                , principal.getMember());
 
         return new ResponseEntity<>(new SingleResponseDto<>(mapper.postsToPostsDtoResponse(response)), HttpStatus.OK);
     }
 
     @GetMapping("/{posts-id}")
-    public ResponseEntity viewPosts(@PathVariable("posts-id") @Positive Long postId,
-                                    @RequestParam(value = "page") int replyPage,
-                                    @RequestParam(value = "size") int replySize) {
+    public ResponseEntity viewPosts(@PathVariable("posts-id") @Positive Long postId) {
 
         Posts response = postsService.lookPosts(postId);
 
-
-        System.out.println("# post 시작 =======================");
         return new ResponseEntity<>(mapper.postsToPostsDtoResponse(response),HttpStatus.OK);
     }
 
@@ -83,7 +76,6 @@ public class PostsController {
                 new MultiResponseDto<>(mapper.postsToPostsDtoPostPageResponses(posts), pagePosts),
                 HttpStatus.OK);
     }
-
 
     @DeleteMapping("/{posts-Id}")
     public ResponseEntity deletePosts(@PathVariable("posts-Id") @Positive Long postId, @AuthenticationPrincipal PrincipalDetails principal) {
