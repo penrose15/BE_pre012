4f28d2a00ca04efbfe4a7dad23411af5a7809d5b
penrose15 <seongju99@naver.com>
2022-09-05 15:32:18 +0900

    access token 만료시간 : 100분

--- src/main/java/com/codestates/pre012/config/filter/JwtProperties.java
+++ src/main/java/com/codestates/pre012/config/filter/JwtProperties.java
@@ -2,7 +2,7 @@ package com.codestates.pre012.config.filter;
 
 public interface JwtProperties {
 	String SECRET = "BE012"; // 우리 서버만 알고 있는 비밀값
-	int EXPIRATION_TIME = 600000; // 10분
+	int EXPIRATION_TIME = 6000000; // 100분
 
 	int REFRESH_EXPIRATION_TIME = 60*1000*60*24;
 	String TOKEN_PREFIX = "Bearer ";
